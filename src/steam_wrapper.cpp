8ff29952e136dcd962c125ef68b9bc5c8511a756
Ciprian <ciliesj@gmail.com>
2015-06-06 19:23:01 -0700

    + SteamAPI_RestartAppIfNecessary()  + SteamUser_BLoggedOn()

--- src/steam_wrapper.cpp
+++ src/steam_wrapper.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 CIPRIAN ILIES
+ * See LICENSE for details.
+ */
+
 namespace steam
 {
 	#include <steam/steam_api.h>
@@ -16,4 +21,20 @@ bool SteamAPI_Init()
 void SteamAPI_Shutdown()
 {
 	steam::SteamAPI_Shutdown();
+}
+
+bool SteamAPI_RestartAppIfNecessary(uint32_t unOwnAppID)
+{
+	return steam::SteamAPI_RestartAppIfNecessary(unOwnAppID);
+}
+
+//What is this? Where is it defined? How do I wrap it? AAAAAAAAAAAAH.
+/*bool Steamworks_InitCEGLibrary()
+{
+	return steam::Steamworks_InitCEGLibrary();
+}*/
+
+bool SteamUser_BLoggedOn()
+{
+	return steam::SteamUser()->BLoggedOn();
 }
--- src/steam_wrapper.h
+++ src/steam_wrapper.h
@@ -1,9 +1,23 @@
+/*
+ * Copyright (c) 2015 CIPRIAN ILIES
+ * See LICENSE for details.
+ */
+
 #ifndef STEAM_WRAPPER_H
 #define STEAM_WRAPPER_H
+
+#include <stdint.h>
 
 //Stuff.
 bool SteamAPI_Init();
 
 void SteamAPI_Shutdown();
+
+bool SteamAPI_RestartAppIfNecessary(uint32_t unOwnAppID);
+
+//What is this? Where is it defined? How do I wrap it? AAAAAAAAAAAAH.
+//bool Steamworks_InitCEGLibrary();
+
+bool SteamUser_BLoggedOn();
 
 #endif
